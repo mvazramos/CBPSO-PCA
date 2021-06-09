7a7c5ed860730e6778de64b266551fbf3490ece3
mvazramos <60268885+mvazramos@users.noreply.github.com>
2021-06-09 13:46:32 +0100

    Add files via upload

---
+++ CBPSO-PCA.R
@@ -0,0 +1,185 @@
+source("cbpsopca_functions.R")
+PSO <- function(X,sX=0,Q=Q,n_Par=500,nIter=30,minIner=0.5,maxIner=3.5,wCognition=1.5,wSocial=1.5){
+  
+  I <- dim(X)[1]
+  J <- dim(X)[2]
+  
+  if(sX==0){
+    "
+    This performs the standardization as it is defined in Macedo and Freitas (2015).
+    "
+    X <- scale(X,center=T,scale=T)
+    X <- as.matrix(X)
+    X <- as.matrix(X*sqrt((I-1)/(I))) 
+  }
+  if(sX==1){
+    "
+    Standardization as assumed in Vigneau et al. and Ramirez-Figueroa et al.
+    "
+    X <- scale(X,center=T,scale=T)
+    X <- as.matrix(X)
+  }
+  
+  if(sX==2){
+    "
+    Only centers the data, as it can also be considered in the works of Ramirez-Figueroa et al. 
+    "
+    X <- scale(X,center=T,scale=F)
+    X <- as.matrix(X)
+  }
+  
+  #################
+  Vel=list()      # all the velocities for each particle
+  
+  Vp=list()   # all the V matrix for each particle 
+  Ap=list()   # all the A matrix for each particle 
+  
+  Vp_best=list()  # the best V matrix for each particle
+  Ap_best=list()  # the best A matrix for each particle
+ 
+  
+  Ap_Fit=c() # All the fit for each particle
+  Ap_bestFit=c() # All the best fit for each particle
+  
+  # Step 1 - Initilization
+  
+  for (i in 1:n_Par) {
+    # Compute loading matrix A_p
+    # Compute fit of Vp ---> Fit(A_p)
+    #Local + Local bests
+    Vp[[i]]=RandMat(J,Q) # Matrix
+    Vp_best[[i]]=Vp[[i]]
+    Ap[[i]]=compute_A(I,J,Q,Vp_best[[i]],X) # Matrix
+    Ap_best[[i]]=Ap[[i]]
+    
+    # Compute each fit 
+    Ap_Fit[i]= fitF(Ap[[i]],X)  # double
+    Ap_bestFit[i]=Ap_Fit[i]
+    
+    Vel[[i]]=randVel(J,Q) # Matrix
+  }
+  
+  
+  #Global 
+  
+  # Get index with best fit:
+  p <- which(Ap_bestFit==min(Ap_bestFit))[1]    # the index that identifies the best particle 
+
+  # Compute global best
+   
+  
+  Vstar <- Vp_best[[p]]    # the V for the best particle
+  Astar <-Ap_best[[p]]     # the A for the best particle
+  FVstar <- fitF(Ap_best[[p]],X)  # the Fit for the best Particle 
+  
+  
+# Iteration and Update 
+  inertia=maxIner
+  for (k in 1:nIter) {
+    #print(FVstar)
+    for (p in 1:n_Par) {
+      #Iteration
+      # newVelocity;
+      rho1=runif(1,0,1)
+      rho2=runif(1,0,1)
+      newVelp= inertia*Vel[[p]] + rho1*wCognition*(Ap_best[[p]]-Ap[[p]])+rho2*wSocial*(Astar-Ap[[p]])
+      
+      Vel[[p]]= operatorL(M=newVelp) # Make velocity between [-1,1]
+      Vtemp= Ap[[p]]+ Vel[[p]]       # New Position Vtemp
+      Vtemp=operatorL(M=Vtemp)
+      
+      # Compute valid feasible solution
+      Vp[[p]]=operatorO(Vtemp,Vel=Vel[[p]],J,Q) # Make Vtemp a valid solution with operator O
+      
+      # Compute A
+      Ap[[p]]=compute_A(I,J,Q,Vp[[p]],X)
+      # Compute fit o A
+      Ap_Fit[p]= fitF(Ap[[p]],X)
+      
+      #Updating
+      
+      if (Ap_Fit[p]<Ap_bestFit[p]) {  # Faz update do Best de cada particula
+        
+        Vp_best[[p]]=Vp[[p]]       # Matrix V
+        Ap_best[[p]]=Ap[[p]]       # Matrix A
+        Ap_bestFit[p]=Ap_Fit[p]
+      }
+      
+      if (Ap_Fit[p]<FVstar ) {       # Faz update do Best Global 
+        Vstar <- Vp[[p]]    # the V for the best particle
+        Astar <-Ap[[p]]     # the A for the best particle
+        FVstar <- Ap_Fit[p]  # the best Fit value for the best particle
+        
+      }
+      
+    }
+    
+    inertia= maxIner- ((maxIner-minIner)/nIter)*k
+    
+  }
+  
+  
+  if(sX==0){ 
+    "
+      In the case of Macedo & Freitas(2015) standardization.
+      
+    "
+  Y <- X%*%Astar
+  varPSO<- var(Y)
+  d <- round(diag(varPSO)*100/J,2)
+  dorder <- d[order(d, decreasing = TRUE)]
+  Resultorder <- t(t(rbind(d,Astar ))[order(d, decreasing = TRUE), ])[-1, ]
+  }
+  else{
+    "
+    Accordingly to the Ramirez-Figueroa et al. considerations.
+    "
+    
+    Y <- X%*%Astar
+    varPSO<- var(Y)
+    d <-round(diag(varPSO)*100/(sum(diag(var(X)))),2)
+    dorder <- d[order(d, decreasing = TRUE)]
+    Resultorder <- t(t(rbind(d,Astar ))[order(d, decreasing = TRUE), ])[-1, ]
+  }
+  
+  return(list(as.matrix(Vstar),as.matrix(Resultorder),FVstar,dorder,Y))  
+}
+
+
+
+# Q=3
+# library(haven)
+# data <- read_sav("world_24nations_25nov2006.sav")
+# df <- data[,c("self1","self2","self3","self4","self5","self6","self7","self8","self9","self10")]
+# df<-na.omit(df)
+# Result_GSE <- PSO(X=df,sX=1,Q=Q,n_Par=500,nIter=30,minIner=0.3,maxIner=3.0,wCognition=1.5,wSocial=1.5)
+# Result_GSE[[2]]
+# Result_GSE[[4]]
+# sum(Result_GSE[[4]])
+
+
+
+# library(readxl)
+# df <- read_excel("C_XRF_data_2v6_8Feb06.xls", sheet = "Sheet2")
+# Result_XRF<- PSO(X=df,sX=1,Q=Q,n_Par=100,nIter=30,minIner=0.3,maxIner=2.5,wCognition=1.5,wSocial=2.5)
+# Result_XRF[[2]]
+# Result_XRF[[4]]
+# sum(Result_XRF[[4]])
+
+
+#df <- read.csv(file = 'dados_Letras.csv',sep=";")
+#df <- dados_Letras
+# Result_Letras <- PSO(X=df,sX=1,Q=Q,n_Par=50,nIter=20,minIner=0.8,maxIner=1.2,wCognition=2,wSocial=2)
+# Result_Letras[[2]]
+# Result_Letras[[4]]
+# sum(Result_Letras[[4]])
+
+
+
+
+
+
+
+
+
+
---
+++ cbpsopca_functions.R
@@ -0,0 +1,251 @@
+
+# Fit function
+fitF <- function(A,Xs){
+  #F<-sum(diag(t(Xs-Xs%*%A%*%t(A))%*%(Xs-Xs%*%A%*%t(A))))/(sum(diag(t(Xs)%*%Xs)) ) 
+  
+  F <-(norm(Xs-Xs%*%A%*%t(A), "F")^2/norm(Xs,"F")^2)
+  
+  return(F)
+}
+
+
+
+
+RandMat <- function(J, Q) {
+  U0 <- matrix(0, J, Q)
+  ###########################################################Changes made to implement the situation dim1=dim2 
+  cc=1
+  for ( i in sample(1:Q,Q) ){
+    U0[cc,] <- diag(Q)[i,]
+    cc=cc+1
+  }  # end for    
+  if (Q<J){
+    for (j in (Q+1):J){
+      p <- sample(1:Q, 1)
+      U0[j, p] <- 1
+    } # end for
+  }  # end if
+  U0
+}  # end RandMat function
+
+
+PMlargestEigen <- function(M){
+  # Power Method
+  # M covariance matrix restricted to some attributes
+  #   (symmetric)
+  tol=10^(-5)
+  maxiteig <- 1000
+  #x0 <- cbind(rep(1,nrow(M))) 
+  x0 <- cbind(runif(nrow(M))) #; x0
+  it <- 0 
+  repeat  
+  { 
+    x1 = M %*% x0 ; 
+    x1 = x1 / norm(x1, "F") #; x1; x1[1] <- -1 ; x1 = x1 / norm(x1, "F"); x1
+    
+    if (norm(abs(x1 - x0), "F") <= tol ) break
+    x0 <- x1 
+    it <- it + 1 
+    if (it == maxiteig) break 
+  }  
+  lambda = sum((M %*% x1) * x1) 
+  list(vector = x1, value = lambda, iterations = it) 
+} 
+
+
+compute_A <- function(I,J,Q,V,Xs){
+  
+  A <- matrix(0, J, Q)
+  
+  for (j in 1: Q) {
+    
+    Jxx <- which(V[, j] == 1)
+    len.Jxx <- length(Jxx)
+    
+    if (sum(V[, j])>1) {
+      if (I >= len.Jxx) {
+        # CASE 1: I >= J (more observations than variables)
+        S.group <- t(Xs[, Jxx])%*%Xs[, Jxx] 
+        #
+        #NEW
+        #          
+        #         A[Jxx, j] <- eigen(S.group)$vectors[, 1]
+        A[Jxx, j] <- PMlargestEigen(S.group)$vector
+        
+        
+      }else{
+        # CASE 2: I < J ( more variables than observations)
+        SS.group <- Xs[, Jxx]%*%t(Xs[, Jxx])         # O que faz isto??  May 7 2021 Question
+        ##          A[Jxx, j] <- t(X.group[, Jxx])%*%eigen(SS.group)$vectors[, 1]/sqrt(eigen(SS.group)$values[1])  #Esta linha do CRAN ? substitu?da pela seguinte:
+        PMinitial <- PMlargestEigen(SS.group)
+        #PMinitial <- partial_eigen(SS.group,1)
+        
+        A[Jxx, j] <- t(Xs[, Jxx])%*%PMinitial$vector/sqrt(PMinitial$value)
+      }  # end if	
+    }else{
+      A[Jxx, j] <- 1
+    }  # end if
+  }  
+  
+  return(A)
+}
+
+
+
+
+
+
+
+
+
+################ General PSO 
+"
+Parameters
+  P: number of particles
+  nIter: number of iterations
+  minIner: minimum of intertia value
+  maxIner: maximum of inertia value
+  wC: cognitive weight
+  wS: social weight
+"
+
+
+
+"
+Each Particle is composed by a matrix :
+  - V:  with de variable configurations   #Position
+  - vel: with the velocities              #Velocity
+"
+
+
+
+
+
+
+
+
+
+
+################################################# Operator to get Valid Solutions 
+randVel <- function(J,Q){
+  "
+  Initiates random velocity matrix with entries between [-1,1]
+  "
+  Vr <- matrix(0, J,Q)
+  for(i in 1:J){
+    for(j in 1:Q){
+      rand <- runif(1,-1,1)
+      Vr[i,j]<- rand
+      
+      
+    }
+  }
+  return(Vr)
+}
+
+
+
+
+
+
+
+
+
+
+operatorO <- function(Mat,Vel,J,Q){
+  
+  "
+                This is the O() operator that converts V(t+1) = V(t) + velocity 
+                to a feasable solution!
+  "
+  tempMat= matrix(0,J,Q)
+  
+  Mat=abs(Mat)
+  for (j in 1:J) {
+    k <- which(Mat[j,]==max(Mat[j,]))
+    tempMat[j,k]=1
+  }
+  tempMat <- checkColZeros(tempMat,Vel)
+  return(tempMat)
+}
+
+
+operatorL <- function(Mat){
+  M=Mat
+  
+  "
+  To guarantee that the entries of the newvelocity is in the interval [-1,1]
+  "
+  Lz <- function(x) {
+    return(2*(1+exp(1)^(-x))^(-1)-1)
+  } 
+  newMat=apply(M, 2, Lz)
+  return(newMat)
+}
+
+#A=operatorO(Mat=randVel(J,Q),J,Q)
+#Vel=A[[1]]
+
+#TestMat = A[[2]]
+#print(TestMat)
+
+checkColZeros <- function(test,Vel){
+  "
+                  Function that makes sure that V is a valid matrix: binary-row stochastic 
+                  Inputs:
+                  - test: V matrix to check and correct
+  "  
+  Mat=test
+  
+  colunas <- colSums(Mat)   # Da a soma de cada coluna
+  
+  
+  
+  if(is.element(0,colunas)){
+    
+    lista <- which(colunas==0)   # Diz-me a coluna de zeros
+    
+    
+    if(length(lista)==1){
+      
+      # Caso com apenas uma coluna cheia de zeros  
+      
+      col_zeros=lista  # indice da coluna com mais zeros
+      max <- which(colunas==max(colunas))[1] # Diz-me a coluna com mais 1's
+      lista_uns <- which(Mat[,max]==1) # Diz as linhas que tem 1's 
+      i <- which(Vel[lista_uns,max]==min(abs(Vel[lista_uns,max])))[1]  # Diz-me a linha associada a menor velocidade 
+      
+      # Correção dos zeros
+      Mat[i,col_zeros]=1
+      Mat[i,max]=0
+    } 
+    else{
+      
+      # Caso com mais do que uma coluna com zeros
+      temp_list=lista
+      count=0
+      
+      while(length(temp_list)!=0){
+        count=count+1
+        col_zeros=temp_list[1]   # indice da coluna com mais zeros 
+        max <- which(colunas==max(colunas))[1] # Diz-me a coluna com mais 1's
+        lista_uns <- which(Mat[,max]==1) # Diz as linhas que tem 1's 
+        
+        i <- which(Vel[lista_uns,max]==min(abs(Vel[lista_uns,max])))  # Diz-me a linha associada a menor velocidade 
+        
+        # Correção dos zeros
+        Mat[i,col_zeros]=1
+        Mat[i,max]=0
+        
+        temp_list=temp_list[-1]
+      }
+      
+      
+    }
+    
+    
+    
+  }
+  
+  return(Mat)
+}
